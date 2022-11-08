70939d2c96358a142f1c662574174c66e7f323e5
jun6988 <114986356+jun6988@users.noreply.github.com>
2022-11-08 09:57:06 +0900

    guestbook #2

---
+++ guestbook01/src/main/java/com/bitacademy/guestbook/dao/GuestbookDao.java
@@ -0,0 +1,117 @@
+package com.bitacademy.guestbook.dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.bitacademy.guestbook.vo.GuestbookVo;
+
+public class GuestbookDao {
+	public Boolean insert(GuestbookVo vo) {
+		boolean result = false;
+		
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		
+		try {
+			Class.forName("org.mariadb.jdbc.Driver");
+			
+			String url = "jdbc:mysql://127.0.0.1:3306/webdb?charset=utf8";
+			conn = DriverManager.getConnection(url, "webdb", "webdb");
+			
+			String sql = "insert into guestbook values(null, ?, ?, ?, now())";
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setString(1, vo.getName());
+			pstmt.setString(2, vo.getPassword());
+			pstmt.setString(3, vo.getContents());
+			
+			int count = pstmt.executeUpdate();
+			
+			//5. 결과 처리
+			result = count == 1;
+			
+		} catch (ClassNotFoundException e) {
+			System.out.println("드라이버 로딩 실패:" + e);
+		} catch (SQLException e) {
+			System.out.println("Error:" + e);
+		} finally {
+			try {
+				if(pstmt != null) {
+					pstmt.close();
+				}
+				
+				if(conn != null) {
+					conn.close();
+				}
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+		
+		return result;
+	}
+	
+	public List<GuestbookVo> findAll() {
+		List<GuestbookVo> result = new ArrayList<>();
+	
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		
+		try {
+			Class.forName("org.mariadb.jdbc.Driver");
+			
+			String url = "jdbc:mysql://127.0.0.1:3306/webdb?charset=utf8";
+			conn = DriverManager.getConnection(url, "webdb", "webdb");
+			
+			String sql =
+				"    select no, name, contents, date_format(reg_date, '%Y/%m/%d %H:%i:%s')" + 
+				"      from guestbook" + 
+				"  order by reg_date desc";
+			pstmt = conn.prepareStatement(sql);
+			
+			rs = pstmt.executeQuery(sql);
+			while(rs.next()) {
+				Long no = rs.getLong(1);
+				String name = rs.getString(2);
+				String contents = rs.getString(3);
+				String regDate = rs.getString(4);
+				
+				GuestbookVo vo = new GuestbookVo();
+				vo.setNo(no);
+				vo.setName(name);
+				vo.setContents(contents);
+				vo.setRegDate(regDate);
+				
+				result.add(vo);
+			}
+			
+		} catch (ClassNotFoundException e) {
+			System.out.println("드라이버 로딩 실패:" + e);
+		} catch (SQLException e) {
+			System.out.println("Error:" + e);
+		} finally {
+			try {
+				if(rs != null) {
+					rs.close();
+				}
+				
+				if(pstmt != null) {
+					pstmt.close();
+				}
+				
+				if(conn != null) {
+					conn.close();
+				}
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}		
+		
+		return result;
+	}
+}
--- guestbook01/src/main/webapp/index.jsp
+++ guestbook01/src/main/webapp/index.jsp
@@ -1,18 +1,24 @@
+<%@page import="com.bitacademy.guestbook.vo.GuestbookVo"%>
+<%@page import="java.util.List"%>
+<%@page import="com.bitacademy.guestbook.dao.GuestbookDao"%>
 <%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
+<%
+	List<GuestbookVo> list = new GuestbookDao().findAll();
+%>
 <html>
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
 <title>방명록</title>
 </head>
 <body>
-	<form action="" method="post">
+	<form action="add.jsp" method="post">
 	<table border=1 width=500>
 		<tr>
 			<td>이름</td><td><input type="text" name="name"></td>
 			<td>비밀번호</td><td><input type="password" name="password"></td>
 		</tr>
 		<tr>
-			<td colspan=4><textarea name="message" cols=60 rows=5></textarea></td>
+			<td colspan=4><textarea name="contents" cols=60 rows=5></textarea></td>
 		</tr>
 		<tr>
 			<td colspan=4 align=right><input type="submit" VALUE=" 확인 "></td>
@@ -20,16 +26,29 @@
 	</table>
 	</form>
 	<br>
-	<table width=510 border=1>
-		<tr>
-			<td>[1]</td>
-			<td>안대혁</td>
-			<td>2013-01-15</td>
-			<td><a href="deleteform.jsp?no=2">삭제</a></td>
-		</tr>
-		<tr>
-			<td colspan=4>안녕하세요</td>
-		</tr>
-	</table>
+	<%
+		int count = list.size();
+		for(GuestbookVo vo : list) {
+	%>
+		<table width=510 border=1>
+			<tr>
+				<td>[<%=count-- %>]</td>
+				<td><%=vo.getName() %></td>
+				<td><%=vo.getRegDate() %></td>
+				<td>
+					<a href="deleteform.jsp?no=<%=vo.getNo() %>">삭제</a>
+				</td>
+			</tr>
+			<tr>
+				<td colspan=4>
+					<%=vo.getContents().replaceAll("\n", "<br>") %>
+				</td>
+			</tr>
+		</table>
+		<br>
+	<%
+		}
+	%>
+	
 </body>
 </html>
